75ed5db813bab5767fe6714b3cb0ccee86708d8d
Henrique Rocha <henrique.rocha@cern.ch>
2020-04-21 22:54:45 +0200

    Improve neural network color

--- src/components/mycanvas/vert.glsl
+++ src/components/mycanvas/vert.glsl
@@ -208,21 +208,27 @@ void main(void) {
             );
             vColor.w = 1.0;
         } else if (uDrawMode == DRAW_MODE_3D_SPHERICAL_GRID) {
+            float geometryVelocity = 0.2;// color changes quicker
+            float geometryFrequency = 100.0;// color is wider
+
             float colorVelocity = 0.2;// color changes quicker
-            float colorFrequency = 100.0;// color is wider
+            float colorFrequency = 0.01;// color is wider
             float angle = abs(atan(vPosition.z, vPosition.x))/PI;
             vec4 audio = texture(uAudioSampler, vec2(angle, 0.0));
             float time = uTime+audio.x;
 
-            float n = noise(vec4(vPosition.x*colorFrequency, vPosition.z*colorFrequency, vPosition.z*colorFrequency, time*colorVelocity));
-            float nx = noise(vec4(vPosition.y*colorFrequency, vPosition.x*colorFrequency, vPosition.z*colorFrequency, time*colorVelocity));
-            float ny = noise(vec4(vPosition.z*colorFrequency, vPosition.x*colorFrequency, vPosition.y*colorFrequency, time*colorVelocity));
-            float nz = noise(vec4(vPosition.z*colorFrequency, vPosition.y*colorFrequency,vPosition.x*colorFrequency, time*colorVelocity));
+            float g = noise(vec4(vPosition.x*geometryFrequency, vPosition.z*geometryFrequency, vPosition.z*geometryFrequency, time*geometryVelocity));
+            float gx = noise(vec4(vPosition.y*geometryFrequency, vPosition.x*geometryFrequency, vPosition.z*geometryFrequency, time*geometryVelocity));
+            float gy = noise(vec4(vPosition.z*geometryFrequency, vPosition.x*geometryFrequency, vPosition.y*geometryFrequency, time*geometryVelocity));
+            float gz = noise(vec4(vPosition.z*geometryFrequency, vPosition.y*geometryFrequency,vPosition.x*geometryFrequency, time*geometryVelocity));
 
-            if(n<0.2){
+            float cx = noise(vec4(vPosition.y*colorFrequency, vPosition.x*colorFrequency, vPosition.z*colorFrequency, time*colorVelocity));
+            float cy = noise(vec4(vPosition.z*colorFrequency, vPosition.x*colorFrequency, vPosition.y*colorFrequency, time*colorVelocity));
+
+            if(g<0.2){
                 vColor = vec4(0.0);
             }else {
-                vColor.xyz = hsv2rgb(vec3(nx, ny*0.25+0.75, 1.0));
+                vColor.xyz = hsv2rgb(vec3(cx, cy*0.25+0.75, 1.0));
                 vColor.w = 1.0;
             }
 
@@ -231,9 +237,9 @@ void main(void) {
             float displacementFrequency = 0.1;
             float displacementVelocity = 2.0;
             vec4 displacement = displacementAmplitude * vec4(
-                nx-0.5,
-                ny-0.5,
-                nz-0.5,
+                gx-0.5,
+                gy-0.5,
+                gz-0.5,
                 0.0
             );
             vPosition += displacement;
